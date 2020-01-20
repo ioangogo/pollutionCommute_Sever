024fcbcef649ddc472ef1ccf1fe6c0b9243bc228
Ioan Loosley <legit.ioangogo@gmail.com>
2020-01-20 19:56:06 +0000

    some config things

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_login import LoginManager 
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.middleware.proxy_fix import ProxyFix
-from . import defultcfg
 
 db = SQLAlchemy()
 
@@ -33,7 +32,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.cfg', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
