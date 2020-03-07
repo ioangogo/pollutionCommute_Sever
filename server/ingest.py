36121a0b52df4af9f6da4405a0d74171a2fc258b
Ioan Loosley <legit.ioangogo@gmail.com>
2020-03-07 12:31:13 +0000

    stupid mistake

--- server/ingest.py
+++ server/ingest.py
@@ -21,7 +21,7 @@ def packetCheck(json, deviceEUI):
         nonceCheck = Recording.query.filter_by(Recording.date_time.after(datetime.datetime.now - datetime.timedelta(days=1)), sensor=sensor, nonce=nonce)
         if len(nonceCheck) == 0:
             nonceValid = True
-        elif nonceCheck.first() is "":
+        elif nonceCheck.first() == "":
             nonceValid = True
     else:
         nonceValid = True
@@ -64,14 +64,14 @@ def sensorIn():
         data = request.json
         deviceEUI = data['sensor']
         sensor = Sensor.query.filter_by(sensorEUI=deviceEUI).first()
-        timestamp = datetime.utcfromtimestamp(int(data['time']))
+        timestamp = datetime.datetime.utcfromtimestamp(int(data['time']))
         sensor, packetValid = packetCheck(data, deviceEUI)
         if packetValid:
             nonce = data['data']['nonce']
             lat = data['data']['lat']
             lng = data['data']['lng']
             pm25 = data['dara']['pm25']
-            Recording(lat=lat, lng=lng, date_time=date, sensor=deviceEUI, pm25=pm25, nonce=nonce)
+            Recording(lat=lat, lng=lng, date_time=timestamp, sensor=deviceEUI, pm25=pm25, nonce=nonce)
         else:
             print("Recording already recived")
             
